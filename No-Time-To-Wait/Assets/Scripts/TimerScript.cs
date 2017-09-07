31281b27b626fbff6fbfcf9c91a5879ed1ea9915
jhonataHunter <jhonatamq@hotmail.com>
2017-09-07 13:55:35 +0100

    GamesFleadh
    GamesFleadh

---
+++ No-Time-To-Wait/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour {
+
+    public float time = 25.0f;               //Time in seconds
+    private float originalTime;
+    public float firstOffTime;
+    public bool done = false;
+    public bool table;
+    public Transform imageToEdit;
+
+    void Start()
+    {
+        originalTime = time;
+        if(table)
+            time = firstOffTime;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(time > 0 && !done)
+        {
+            time -= Time.deltaTime;
+            imageToEdit.GetComponent<Image>().fillAmount = (time / originalTime);
+        }
+        else
+        {
+            done = true;
+            time = originalTime;
+        }
+    }
+}
